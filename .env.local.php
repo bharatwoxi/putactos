968a820f2f193021c0ef7bfa2eb1c612b7beab35
Sagar Acharya <sagar.woxi@gmail.com>
2014-12-08 17:30:07 +0530

    user registration

--- .env.local.php
+++ .env.local.php
@@ -5,5 +5,6 @@ return array(
     'DB_PASSWORD' => 'root',
     'DB_NAME' => 'putactos',
     'SP_FILE_UPLOAD_PATH' => public_path().'/uploads/userdata/service_provider',
+    'CUSTOMER_FILE_UPLOAD_PATH' => public_path().'/uploads/userdata/customer',
 );
 ?>
