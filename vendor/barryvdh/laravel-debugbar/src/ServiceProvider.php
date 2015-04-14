bd3ba8c649d939ef780bcca2065d3a6f483ed181
Sagar Acharya <sagar.acharya@weboniselab.com>
2015-04-14 11:53:36 +0530

    composer update

--- vendor/barryvdh/laravel-debugbar/src/ServiceProvider.php
+++ vendor/barryvdh/laravel-debugbar/src/ServiceProvider.php
@@ -17,6 +17,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     public function boot()
     {
         $app = $this->app;
+
         $app['config']->package('barryvdh/laravel-debugbar', __DIR__ . '/config');
 
         if ($app->runningInConsole()) {
@@ -72,13 +73,6 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         }
     }
 
-    protected function shouldUseMiddleware()
-    {
-        $app = $this->app;
-        $version = $app::VERSION;
-        return !$app->runningInConsole() && version_compare($version, '4.1-dev', '>=') && version_compare($version, '5.0-dev', '<');
-    }
-
     /**
      * Register the service provider.
      *
@@ -105,9 +99,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 
         $this->app['command.debugbar.publish'] = $this->app->share(
             function ($app) {
-                //Make sure the asset publisher is registered.
-                $app->register('Illuminate\Foundation\Providers\PublisherServiceProvider');
-                return new Console\PublishCommand($app['asset.publisher']);
+                return new Console\PublishCommand();
             }
         );
 
@@ -122,6 +114,18 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         if ($this->shouldUseMiddleware()) {
             $this->app->middleware('Barryvdh\Debugbar\Middleware\Stack', array($this->app));
         }
+    }
+
+    /**
+     * Detect if the Middelware should be used.
+     * 
+     * @return bool
+     */
+    protected function shouldUseMiddleware()
+    {
+        $app = $this->app;
+        $version = $app::VERSION;
+        return !$app->runningInConsole() && version_compare($version, '4.1-dev', '>=') && version_compare($version, '5.0-dev', '<');
     }
 
     /**
