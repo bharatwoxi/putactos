df04d9d41e7c5327864044b2a7ffd9da8396e132
Sagar Acharya <woxiapps@gmail.com>
2015-05-30 06:12:04 +0000

    composer

--- .env.php
+++ .env.php
@@ -1,14 +1,17 @@
 <?php
 return array(
     'DB_HOST' => 'localhost',
-    'DB_UNAME' => 'woxiprog_bfw',
-    'DB_PASSWORD' => 'W2V3[uD*)SZi',
-    'DB_NAME' => 'woxiprog_putactos',
+    'DB_UNAME' => 'root',
+    'DB_PASSWORD' => 'Woxi@123890',
+    'DB_NAME' => 'putactos',
     'SP_FILE_UPLOAD_PATH' => public_path().'/uploads/userdata/service_provider',
     'CUSTOMER_FILE_UPLOAD_PATH' => public_path().'/uploads/userdata/customer',
     'SP_FILE_VIEW_PATH' => 'public/uploads/userdata/service_provider',
     'CUSTOMER_FILE_VIEW_PATH' => 'public/uploads/userdata/customer',
     'reCaptchSiteKey' => '6LdGsf8SAAAAALW2n4ZZEjMLK24t5DniyUOFRSwP',
     'reCaptchaSecretKey' => '6LdGsf8SAAAAAF9upsJF1_UySn3JTUfphKXiVGmE',
+    'SWEETCAPTCHA_APP_ID'=> '238346',
+    'SWEETCAPTCHA_KEY'=> '3659f467af1d4aa03a94a6d0188c161e',
+    'SWEETCAPTCHA_SECRET'=>'687c98b8cad54ba464ed3ccaa65bb1b1'
 );
 ?>
