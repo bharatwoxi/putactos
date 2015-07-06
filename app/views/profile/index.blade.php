c40e97cc8d38aaa1742c318777e3f3044b4fda93
Suraj Bande <suraj.woxi@gmail.com>
2015-07-06 19:45:15 +0530

    ui change for footer

--- app/views/profile/index.blade.php
+++ app/views/profile/index.blade.php
@@ -459,7 +459,7 @@
 <div class="col-lg-12 conter">
     <div class="container">
         <div class="row" style="margin:30px 0">
-            <div class="col-md-3 col-xs-offset-1">
+            <div class="col-md-4 col-xs-offset-1">
                 <h4 class="hfour">CONNECT WITH US</h4>
                 <a href="#" ><img src="img/youtube.png" style="width:52px; margin: 0 15px 10px 0px"></a>
                 <a href="#" ><img src="img/social.png" style="width:52px; margin: 0 15px 10px 0px"></a>
@@ -469,7 +469,7 @@
                 <a href="#" ><img src="img/linkedin.png" style="width:52px; margin: 0 15px 10px 0px"></a>
                 <a href="#" ><img src="img/facebook.png" style="width:52px; margin: 0 0 10px 0px"></a>
             </div>
-            <div class="col-md-4" style="border-left: 2px solid #0f0f0f; padding-left: 70px;">
+            <div class="col-md-4" style="border-left: 2px solid #0f0f0f;">
                 <h4 class="hfour">GET IN TOUCH</h4>
                 <p><a href="#" ><img src="img/tele.png" style="width:52px"></a>         <strong>1-800-355-2626</strong> </p>
                 <P><a href="#" ><img src="img/msg.png" style="width:52px"></a>      <strong>abc@putactos.com</strong>
